c68ae8318f67da3e0f25f85ab1fed94bd6e65622
Teancum Besendorfer <teancum.besendorfer@outlook.com>
2017-01-30 20:51:47 -0800

    Indent using 2 spaces instead of tabs size 4

--- scanner.js
+++ scanner.js
@@ -1,45 +1,45 @@
 'use strict';
 
 (function () {
-	var root = this
+  var root = this
 
-	var hasRequire = typeof require !== 'undefined'
+  var hasRequire = typeof require !== 'undefined'
 
-	// Include required dependencies.
-	// Find a better format to do this? This seems quite bloated...
-	var q = root.q
+  // Include required dependencies.
+  // Find a better format to do this? This seems quite bloated...
+  var q = root.q
 
-	if (typeof q === 'undefined') {
-		if(hasRequire) {
-			q = require('q')
-		} else {
-			throw new Error('scanner requires q. Please install.')
-		}
-	}
+  if (typeof q === 'undefined') {
+    if(hasRequire) {
+      q = require('q')
+    } else {
+      throw new Error('scanner requires q. Please install.')
+    }
+  }
 
-	var netPing = root.netPing
+  var netPing = root.netPing
 
-	if (typeof netPing === 'undefined') {
-		if (hasRequire) {
-			netPing = require('net-ping')
-		} else {
-			throw new Error('scanner requires net-ping. Please install.')
-		}
-	}
+  if (typeof netPing === 'undefined') {
+    if (hasRequire) {
+      netPing = require('net-ping')
+    } else {
+      throw new Error('scanner requires net-ping. Please install.')
+    }
+  }
 
-	// Implementation constructor
-	var scanner = function () {
-		
-	}
+  // Implementation constructor
+  var scanner = function () {
+    
+  }
 
-	if (typeof exports !== 'undefined') {
-		if (typeof module !== 'undefined' && module.exports) {
-			exports = module.exports = scanner
-		}
-		exports.scanner = scanner
-	} else {
-		root.scanner = scanner
-	}
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = scanner
+    }
+    exports.scanner = scanner
+  } else {
+    root.scanner = scanner
+  }
 })()
 
 /////////////////////////// OLD CODE
@@ -50,7 +50,7 @@ const Q = require('q');
 const ping = require('net-ping');
 
 let debug = false;
- 	
+   
 // TODO:	--help for info
 //			Also, make sure that when something weird happens in the arguments, it shows --help
 
@@ -90,19 +90,19 @@ let debug = false;
 //
 // 		--showClosedPorts=true|false	Determines whether to show closed ports in scan results (default: true)
 let getArgs = argv => {
-	let usefulArgs = { tcp: 'true', timeout: 2000, showClosedPorts: 'true' };
+  let usefulArgs = { tcp: 'true', timeout: 2000, showClosedPorts: 'true' };
 
-	argv.slice(2).forEach(data => {
-		if (data.indexOf('--') !== -1) {
-			let equals = data.indexOf('=');
+  argv.slice(2).forEach(data => {
+    if (data.indexOf('--') !== -1) {
+      let equals = data.indexOf('=');
 
-			usefulArgs[data.slice(2, equals)] = data.slice(equals + 1);
-		}
-	});
+      usefulArgs[data.slice(2, equals)] = data.slice(equals + 1);
+    }
+  });
 
-	debug && console.log(usefulArgs);
+  debug && console.log(usefulArgs);
 
-	return usefulArgs;
+  return usefulArgs;
 };
 
 // Doesn't currently allow for subnets. I'll probably make the subnet super simple
@@ -111,23 +111,23 @@ let getArgs = argv => {
 // Parses the hosts by either a comma (192.168.1.2,192.168.1.4) or a dash (192.168.1.2-254)
 // Combining them works too (192.168.1.2,192.168.1.100-254)
 let parseHosts = hosts => {
-	debug && console.log(hosts);
+  debug && console.log(hosts);
 
-	if(hosts.indexOf('-') !== -1) {
-		return hosts.split(',').map(host => {
-			if (host.indexOf('-') !== -1) {
-				let firstThree = host.split('.')[0] + '.' + host.split('.')[1] + '.' + host.split('.')[2];
-				let interval = host.split('.')[3].split('-');
-				let length = interval[1] - interval[0] + 1;
+  if(hosts.indexOf('-') !== -1) {
+    return hosts.split(',').map(host => {
+      if (host.indexOf('-') !== -1) {
+        let firstThree = host.split('.')[0] + '.' + host.split('.')[1] + '.' + host.split('.')[2];
+        let interval = host.split('.')[3].split('-');
+        let length = interval[1] - interval[0] + 1;
 
-				return Array.from(Array(length).keys())
-							.map(x => x + parseInt(interval[0]))
-							.map(String)
-							.map(x => firstThree + '.' + x);
-			}
-		}).reduce((first, second) => first.concat(second), []);
-	}
-	else return hosts.split(',');
+        return Array.from(Array(length).keys())
+              .map(x => x + parseInt(interval[0]))
+              .map(String)
+              .map(x => firstThree + '.' + x);
+      }
+    }).reduce((first, second) => first.concat(second), []);
+  }
+  else return hosts.split(',');
 };
 
 // Am I missing anything? Also, a regex probably could have caught everything, but I really
@@ -136,129 +136,129 @@ let parseHosts = hosts => {
 // Just like parsing the hosts, this parses either by comma (22,23,80) or by dash (1-1000)
 // and you can combine them (22,23,80,135-139,443)
 let parsePorts = ports => {
-	debug && console.log(ports);
+  debug && console.log(ports);
 
-	if (ports.indexOf('-') !== -1) {
-		return ports.split(',').map(port => {
-			if (port.indexOf('-') !== -1) {
-				let interval = port.split('-');
-				let length = interval[1] - interval[0] + 1;
+  if (ports.indexOf('-') !== -1) {
+    return ports.split(',').map(port => {
+      if (port.indexOf('-') !== -1) {
+        let interval = port.split('-');
+        let length = interval[1] - interval[0] + 1;
 
-				return Array.from(Array(length).keys())
-							.map(x => x + parseInt(interval[0])).map(String);
-			}
-			return port;
-		}).reduce((first, second) => first.concat(second), []);
-	}
-	else return ports.split(',');
+        return Array.from(Array(length).keys())
+              .map(x => x + parseInt(interval[0])).map(String);
+      }
+      return port;
+    }).reduce((first, second) => first.concat(second), []);
+  }
+  else return ports.split(',');
 };
 
 // TCP Scan the hosts and the ports.
 let tcpScan = (host, port, timeout) => {
-	let socket = net.createConnection(port, host);
-	socket.setTimeout(timeout);
+  let socket = net.createConnection(port, host);
+  socket.setTimeout(timeout);
 
-	let result = { host, port, type: 'TCP' };
+  let result = { host, port, type: 'TCP' };
 
-	let deferred = Q.defer();
+  let deferred = Q.defer();
 
-	// Create callback functions for each possible result.
-	socket.on('error', err => {
-		result.status = {};
+  // Create callback functions for each possible result.
+  socket.on('error', err => {
+    result.status = {};
 
-		if (err.message.indexOf('ECONNREFUSED') !== -1) {
-			result.status.state = 'closed';
-			result.status.reason = 'connection refused';
-		}
-		else if (err.message.indexOf('EHOSTUNREACH') !== -1) {
-			result.status.state = 'closed';
-			result.status.reason = 'host unreachable';
-		}
-		else
-			result.status.state = err.message;
+    if (err.message.indexOf('ECONNREFUSED') !== -1) {
+      result.status.state = 'closed';
+      result.status.reason = 'connection refused';
+    }
+    else if (err.message.indexOf('EHOSTUNREACH') !== -1) {
+      result.status.state = 'closed';
+      result.status.reason = 'host unreachable';
+    }
+    else
+      result.status.state = err.message;
 
-		debug && console.log('error');
-	});
-	socket.on('timeout', () => {
-		result.status = {};
+    debug && console.log('error');
+  });
+  socket.on('timeout', () => {
+    result.status = {};
 
-		if (result.isOpen) result.status.state = 'open';
-		else {
-			result.status.state = 'closed';
-			result.status.reason = 'connection timed out';
-		}
+    if (result.isOpen) result.status.state = 'open';
+    else {
+      result.status.state = 'closed';
+      result.status.reason = 'connection timed out';
+    }
 
-		debug && console.log('timeout');
+    debug && console.log('timeout');
 
-		socket.destroy();
-	});
-	socket.on('connect', () => {
-		result.isOpen = true;
-		debug && console.log('connect');
-	});
-	socket.on('data', data => {
-		// This might be useful later to get port service information
-		result.receivedData = true;
+    socket.destroy();
+  });
+  socket.on('connect', () => {
+    result.isOpen = true;
+    debug && console.log('connect');
+  });
+  socket.on('data', data => {
+    // This might be useful later to get port service information
+    result.receivedData = true;
 
-		debug && console.log('data');
+    debug && console.log('data');
 
-		socket.destroy();
-	});
-	socket.on('close', () => {
-		if (!result.receivedData && result.isOpen) {
-			result.status = {};
-			result.isOpen = false;
-			result.status.state = 'open';
-			result.status.reason = 'no data received';
-		}
+    socket.destroy();
+  });
+  socket.on('close', () => {
+    if (!result.receivedData && result.isOpen) {
+      result.status = {};
+      result.isOpen = false;
+      result.status.state = 'open';
+      result.status.reason = 'no data received';
+    }
 
-		socket.destroy();
+    socket.destroy();
 
-		debug && console.log('close');
+    debug && console.log('close');
 
-		// It appears that 'close' is always called at the end, so
-		// I feel that putting the deferred.resolve here is probably
-		// sufficient.
-		deferred.resolve(result);
-	});
+    // It appears that 'close' is always called at the end, so
+    // I feel that putting the deferred.resolve here is probably
+    // sufficient.
+    deferred.resolve(result);
+  });
 
-	return deferred.promise;
+  return deferred.promise;
 };
 
 // The udp version of the scan. It is super simple, and probably doesn't
 // work very well. But it is the best I could find. Node.js doesn't really
 // have any other tools available.
 let udpScan = (host, port) => {
-	const buffer = new Buffer('Some bytes');
-	const socket = dgram.createSocket('udp4');
+  const buffer = new Buffer('Some bytes');
+  const socket = dgram.createSocket('udp4');
 
-	let deferred = Q.defer();
-	let result = { host, port, type: 'UDP' };
+  let deferred = Q.defer();
+  let result = { host, port, type: 'UDP' };
 
-	socket.send(buffer, 0, buffer.length, port, host, err => {
-		result.data = err;
-		deferred.resolve(result);
-		socket.close();
-	});
+  socket.send(buffer, 0, buffer.length, port, host, err => {
+    result.data = err;
+    deferred.resolve(result);
+    socket.close();
+  });
 
-	return deferred.promise;
+  return deferred.promise;
 }
 
 // To determine if the machine is alive (not super reliable, but definitely better than nothing)
 // Only runs if the icmp flag is true. Essentially, a ping.
 let icmpScan = host => {
-	let deferred = Q.defer();
-	let result = { host };
+  let deferred = Q.defer();
+  let result = { host };
 
-	const session = ping.createSession();
+  const session = ping.createSession();
 
-	session.pingHost(host, (err, host) => {
-		result.alive = !err;
-		deferred.resolve(result);
-		session.close();
-	});
+  session.pingHost(host, (err, host) => {
+    result.alive = !err;
+    deferred.resolve(result);
+    session.close();
+  });
 
-	return deferred.promise;
+  return deferred.promise;
 };
 
 // Does a traceroute. Unsure how reliable this is, but it seems to be working.
@@ -268,26 +268,26 @@ let icmpScan = host => {
 // Eventually will write flags to allow the user to change the various flags possible
 // with traceroute (timeout, maxHopTimeouts, ttl...)
 let icmpTraceroute = host => {
-	let deferred = Q.defer();
-	let result = { host, type: 'traceroute', route: [] };
-	let order = 0;
+  let deferred = Q.defer();
+  let result = { host, type: 'traceroute', route: [] };
+  let order = 0;
 
-	const session = ping.createSession({
-		retries: 1,
-		timeout: 2000
-	});
+  const session = ping.createSession({
+    retries: 1,
+    timeout: 2000
+  });
 
-	session.traceRoute(host, { ttl: 10, maxHopTimeouts: 10 }, 
-		(err, host, ttl, sent, rcvd) => {
-			let route = { order: ++order };
-			route.ip = err ? err instanceof ping.TimeExceededError ? err.source : err.message : host;
-			result.route.push(route);
-		},
-		(err, host) => {
-			deferred.resolve(result);
-		});
+  session.traceRoute(host, { ttl: 10, maxHopTimeouts: 10 }, 
+    (err, host, ttl, sent, rcvd) => {
+      let route = { order: ++order };
+      route.ip = err ? err instanceof ping.TimeExceededError ? err.source : err.message : host;
+      result.route.push(route);
+    },
+    (err, host) => {
+      deferred.resolve(result);
+    });
 
-	return deferred.promise;
+  return deferred.promise;
 };
 
 // This is where we get the arguments and parse the hosts and ports.
@@ -305,7 +305,7 @@ let udpScans = [];
 
 // This is where the scanning begins.
 hosts.forEach(host => {
-	if (args.icmp === 'true') icmpScans.push(icmpScan(host));
+  if (args.icmp === 'true') icmpScans.push(icmpScan(host));
 });
 
 // Q is necessary because these scans are asynchronous. In order to get the data
@@ -316,60 +316,60 @@ hosts.forEach(host => {
 
 // A lightweight, built in version of Q is available for AngularJS, called $q.
 Q.all(icmpScans).then(icmpScanResults => {
-	let aliveHosts = args.icmp === 'true' ? [] : hosts;
+  let aliveHosts = args.icmp === 'true' ? [] : hosts;
 
-	icmpScanResults.forEach(scanResult => {
-		if (scanResult.alive) aliveHosts.push(scanResult.host);
-	});
+  icmpScanResults.forEach(scanResult => {
+    if (scanResult.alive) aliveHosts.push(scanResult.host);
+  });
 
-	// Gather traceroute, TCP, and UDP scans
-	aliveHosts.forEach(host => {
-		if (args.traceroute === 'true') icmpTraceroutes.push(icmpTraceroute(host));
-	});
+  // Gather traceroute, TCP, and UDP scans
+  aliveHosts.forEach(host => {
+    if (args.traceroute === 'true') icmpTraceroutes.push(icmpTraceroute(host));
+  });
 
-	aliveHosts.forEach(host => {
-		ports.forEach(port => {
-			if (args.tcp === 'true') tcpScans.push(tcpScan(host, port, parseInt(args.timeout)));
-			if (args.udp === 'true') udpScans.push(udpScan(host, port));
-		});
-	});
+  aliveHosts.forEach(host => {
+    ports.forEach(port => {
+      if (args.tcp === 'true') tcpScans.push(tcpScan(host, port, parseInt(args.timeout)));
+      if (args.udp === 'true') udpScans.push(udpScan(host, port));
+    });
+  });
 
-	// Put all the scans together in a single array.
-	let allScans = tcpScans.concat(udpScans).concat(icmpTraceroutes);
+  // Put all the scans together in a single array.
+  let allScans = tcpScans.concat(udpScans).concat(icmpTraceroutes);
 
-	// Print out the results
-	Q.all(allScans).then(results => {
-		debug && console.log(results);
+  // Print out the results
+  Q.all(allScans).then(results => {
+    debug && console.log(results);
 
-		results.sort((a, b) => a.host.split('.')[3] - b.host.split('.')[3])
-		.forEach(result => {
-			// I know that this is messy. I'll be sure to clean this up later.
-			if (args.showClosedPorts === 'true' ||
-				(result.type === 'TCP' && result.isOpen || result.status.state === 'open') ||
-				(result.type === 'UDP' && !(result.data || result.data === 0))) {
-				let output = '';
+    results.sort((a, b) => a.host.split('.')[3] - b.host.split('.')[3])
+    .forEach(result => {
+      // I know that this is messy. I'll be sure to clean this up later.
+      if (args.showClosedPorts === 'true' ||
+        (result.type === 'TCP' && result.isOpen || result.status.state === 'open') ||
+        (result.type === 'UDP' && !(result.data || result.data === 0))) {
+        let output = '';
 
-				if (result.type !== 'traceroute') {
-					output = result.host + ':' + result.port + '\t' + result.type + '\t';
+        if (result.type !== 'traceroute') {
+          output = result.host + ':' + result.port + '\t' + result.type + '\t';
 
-					if (result.type === 'TCP') {
-						if (result.isOpen) output += 'open';
-						else if (result.status) output += result.status.state + ' (' + result.status.reason + ')';
-					}
-					else if (result.type === 'UDP') {
-						if (result.data || result.data === 0) output += 'closed';
-						else output += 'maybe open (no data or error received)';
-					}
-				}
-				else if (result.type === 'traceroute') {
-					output = 'traceroute: ' + result.host + '\n';
+          if (result.type === 'TCP') {
+            if (result.isOpen) output += 'open';
+            else if (result.status) output += result.status.state + ' (' + result.status.reason + ')';
+          }
+          else if (result.type === 'UDP') {
+            if (result.data || result.data === 0) output += 'closed';
+            else output += 'maybe open (no data or error received)';
+          }
+        }
+        else if (result.type === 'traceroute') {
+          output = 'traceroute: ' + result.host + '\n';
 
-					result.route.sort((first, second) => first.order - second.order)
-								.forEach(route => output += route.order.toString() + ' ' + route.ip + '\n');
-				}
+          result.route.sort((first, second) => first.order - second.order)
+                .forEach(route => output += route.order.toString() + ' ' + route.ip + '\n');
+        }
 
-				console.log(output);
-			}
-		});
-	});
+        console.log(output);
+      }
+    });
+  });
 });
